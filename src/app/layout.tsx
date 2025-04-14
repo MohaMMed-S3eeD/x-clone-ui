b1a08bef0798cd94136004c9b25bbb448b3b6af8
Mohamed Seed <mosaeed7888@gmail.com>
2025-04-14 08:04:42 +0200

    Remove modal prop and its usage from RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,10 +11,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  modal,
+  // modal,
 }: Readonly<{
   children: React.ReactNode;
-  modal: React.ReactNode;
+  // modal: React.ReactNode;
 }>) {
 
   
@@ -27,7 +27,7 @@ export default function RootLayout({
           </div>
           <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray ">
             {children}
-            {modal}
+            {/* {modal} */}
           </div>
           <div className="hidden lg:flex ml-4 md:ml-8 flex-1 ">
             <RightBar />
