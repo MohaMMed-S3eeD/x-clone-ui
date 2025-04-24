9733e584f1438579ec550f3d3a40d043f1d9c15d
Mohamed Seed <mosaeed7888@gmail.com>
2025-04-24 17:31:13 +0200

    Refactor RightBar component: integrate Search, PopularTags, and Recommendations components, and enhance footer links

--- src/components/RightBar.tsx
+++ src/components/RightBar.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import Search from "./Search";
+import Recommendations from "./Recommendations";
+import PopularTags from "./PopularTags";
+import Link from "next/link";
 
 const RightBar = () => {
   return (
-    <div>RightBar</div>
-  )
-}
+    <div>
+      <Search  />
+      <PopularTags />
+      <Recommendations />
+      <div className="flex flex-wrap gap-x-3 text-textGray text-sm underline">
+        <Link href="/">Terms of Service</Link>
+        <Link href="/">Privacy Policy</Link>
+        <Link href="/">Cookie Policy</Link>
+        <Link href="/">Accessibility</Link>
+        <Link href="/">Ads Info</Link>
+      </div>
+      <span className=" flex justify-center items-center mt-2 text-textGray text-sm ">
+        © 2025 L Corp.
+      </span>
+    </div>
+  );
+};
 
-export default RightBar
+export default RightBar;
